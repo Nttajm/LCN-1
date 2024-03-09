88d37ae20d8d6d515cfaf637b7e3aa5eba9291ad
cloner <joelmulonde81@gmail.com>
2024-03-09 01:50:57 -0800

    cmt

---
+++ js/PS/systeminfo/systeminfo.js
@@ -0,0 +1,12 @@
+    export const cred = {
+        name : 'Joel Mulonde',
+        qt1: 'visca barca!!!!'
+    }
+
+    export const systemInfo = {
+        systemcheck: 'pass.200',
+        version : 'v.1.0.4',
+        name: 'dbmPs',
+        status: 'semi-opp'
+    }
+
