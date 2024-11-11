4d9454e755d4adad1a967f38627fb394cdff54f1
Nttajm <130328868+Nttajm@users.noreply.github.com>
2024-11-10 22:33:02 -0800

    ctwc update and more

--- ctwc/dad/html/script.js
+++ ctwc/dad/html/script.js
@@ -2,50 +2,50 @@ const peopleImgs = document.querySelectorAll('.people img');
 
 if (peopleImgs.length > 0) {
     const imgUrls = [
-        '/imagesbynum/img (5).jpg',
-        '/imagesbynum/img (6).jpg',
-        '/imagesbynum/img (7).jpg',
-        '/imagesbynum/img (8).jpg',
-        '/imagesbynum/img (9).jpg',
-        '/imagesbynum/img (10).jpg',
-        '/imagesbynum/img (11).jpg',
-        '/imagesbynum/img (12).jpg',
-        '/imagesbynum/img (13).jpg',
-        '/imagesbynum/img (14).jpg',
-        '/imagesbynum/img (15).jpg',
-        '/imagesbynum/img (16).jpg',
-        '/imagesbynum/img (17).jpg',
-        '/imagesbynum/img (18).jpg',
-        '/imagesbynum/img (19).jpg',
-        '/imagesbynum/img (20).jpg',
-        '/imagesbynum/img (21).jpg',
-        '/imagesbynum/img (22).jpg',
-        '/imagesbynum/img (23).jpg',
-        '/imagesbynum/img (24).jpg',
-        '/imagesbynum/img (25).jpg',
-        '/imagesbynum/img (26).jpg',
-        '/imagesbynum/img (27).jpg',
-        '/imagesbynum/img (28).jpg',
-        '/imagesbynum/img (33).jpg',
-        '/imagesbynum/img (34).jpg',
-        '/imagesbynum/img (35).jpg',
-        '/imagesbynum/img (36).jpg',
-        '/imagesbynum/img (37).jpg',
-        '/imagesbynum/img (38).jpg',
-        '/imagesbynum/img (39).jpg',
-        '/imagesbynum/img (40).jpg',
-        '/imagesbynum/img (41).jpg',
-        '/imagesbynum/img (42).jpg',
-        '/imagesbynum/img (43).jpg',
-        '/imagesbynum/img (44).jpg',
-        '/imagesbynum/img (45).jpg',
-        '/imagesbynum/img (46).jpg',
-        '/imagesbynum/img (47).jpg',
-        '/imagesbynum/img (48).jpg',
-        '/imagesbynum/img (49).jpg',
-        '/imagesbynum/img (50).jpg',
-        '/imagesbynum/img (51).jpg',
-        '/imagesbynum/img (52).jpg',
+        '../imagesbynum/img (5).jpg',
+        '../imagesbynum/img (6).jpg',
+        '../imagesbynum/img (7).jpg',
+        '../imagesbynum/img (8).jpg',
+        '../imagesbynum/img (9).jpg',
+        '../imagesbynum/img (10).jpg',
+        '../imagesbynum/img (11).jpg',
+        '../imagesbynum/img (12).jpg',
+        '../imagesbynum/img (13).jpg',
+        '../imagesbynum/img (14).jpg',
+        '../imagesbynum/img (15).jpg',
+        '../imagesbynum/img (16).jpg',
+        '../imagesbynum/img (17).jpg',
+        '../imagesbynum/img (18).jpg',
+        '../imagesbynum/img (19).jpg',
+        '../imagesbynum/img (20).jpg',
+        '../imagesbynum/img (21).jpg',
+        '../imagesbynum/img (22).jpg',
+        '../imagesbynum/img (23).jpg',
+        '../imagesbynum/img (24).jpg',
+        '../imagesbynum/img (25).jpg',
+        '../imagesbynum/img (26).jpg',
+        '../imagesbynum/img (27).jpg',
+        '../imagesbynum/img (28).jpg',
+        '../imagesbynum/img (33).jpg',
+        '../imagesbynum/img (34).jpg',
+        '../imagesbynum/img (35).jpg',
+        '../imagesbynum/img (36).jpg',
+        '../imagesbynum/img (37).jpg',
+        '../imagesbynum/img (38).jpg',
+        '../imagesbynum/img (39).jpg',
+        '../imagesbynum/img (40).jpg',
+        '../imagesbynum/img (41).jpg',
+        '../imagesbynum/img (42).jpg',
+        '../imagesbynum/img (43).jpg',
+        '../imagesbynum/img (44).jpg',
+        '../imagesbynum/img (45).jpg',
+        '../imagesbynum/img (46).jpg',
+        '../imagesbynum/img (47).jpg',
+        '../imagesbynum/img (48).jpg',
+        '../imagesbynum/img (49).jpg',
+        '../imagesbynum/img (50).jpg',
+        '../imagesbynum/img (51).jpg',
+        '../imagesbynum/img (52).jpg',
     ]
 
     peopleImgs.forEach((img) => {
@@ -71,22 +71,22 @@ if (peopleImgs.length > 0) {
     });
 }
 
-    const menuBtn = document.querySelector('.menu');
-    const closeBtn1 = document.querySelector('#close-1');
-    const navContDiv = document.querySelector('.nav-cont');
+const menuBtn = document.querySelector('.menu');
+const closeBtn1 = document.querySelector('#close-1');
+const navContDiv = document.querySelector('.nav-cont');
 
-    if (menuBtn) {
-        menuBtn.addEventListener('click', () => {
-            if (navContDiv) {
-                navContDiv.classList.toggle('show-other-links');
-            }
-        });
-    }
+if (menuBtn) {
+    menuBtn.addEventListener('click', () => {
+        if (navContDiv) {
+            navContDiv.classList.toggle('show-other-links');
+        }
+    });
+}
 
-    if (closeBtn1) {
-        closeBtn1.addEventListener('click', () => {
-            if (navContDiv) {
-                navContDiv.classList.remove('show-other-links');
-            }
-        });
-    }
+if (closeBtn1) {
+    closeBtn1.addEventListener('click', () => {
+        if (navContDiv) {
+            navContDiv.classList.remove('show-other-links');
+        }
+    });
+}
