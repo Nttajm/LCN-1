668d2219590e9ea62dac258142ebeaf53a51a462
joel.m <joelmulonde81@gmail.comm>
2024-11-15 11:02:56 -0800

    Update stock data in stocks-array.js

--- bp/inspo/overunder/stocks-array.js
+++ bp/inspo/overunder/stocks-array.js
@@ -5,7 +5,7 @@ export const stockManual = [
          name: 'Over Under',
          basedOn: 'over under in genral',
          data: [
-             43, 53, 104, 121, 99, 87, 106, 122, 132, 145
+             43, 53, 104, 121, 99, 87, 106, 122, 132, 145, 190,
          ],
     }, 
      
@@ -24,7 +24,7 @@ export const stockManual = [
         name: 'Mylander',
         basedOn: 'Taking phones',
         data: [
-            60, 12, 19, 28, 34, 54, 67, 87, 125, 142, 176, 163, 189, 151, 121
+            60, 12, 19, 28, 34, 54, 67, 87, 125, 142, 176, 163, 189, 151, 121, 109,
         ],
     },
     {
@@ -33,7 +33,7 @@ export const stockManual = [
         name: 'Weaver',
         basedOn: 'sarcasm',
         data: [
-          10,  18.5, 9, 8, 6, 3, 5, 2, 1, 3, 2.3, 2, 3.5, 4.2, 4.6, 5.1, 7.5, 6, 9.5
+          10,  18.5, 9, 8, 6, 3, 5, 2, 1, 3, 2.3, 2, 3.5, 4.2, 4.6, 5.1, 7.5, 6, 9.5, 8
         ],
     },
     {
@@ -42,7 +42,7 @@ export const stockManual = [
         name: 'Burks',
         basedOn: 'Yapping',
         data: [
-             8, 7, 5, 6, 8, 9, 10, 12, 14, 56, 70, 61, 85, 87, 93,96, 101, 85, 94, 99
+             8, 7, 5, 6, 8, 9, 10, 12, 14, 56, 70, 61, 85, 87, 93,96, 101, 85, 94, 99, 71
         ],
     },
     {
@@ -51,7 +51,7 @@ export const stockManual = [
         name: 'Matt Ortiz',
         basedOn: 'YELLING',
         data: [
-            12, 14, 15, 16, 11, 8, 5, 7, 10, 11, 15, 27, 41, 48,
+            12, 14, 15, 16, 11, 8, 5, 7, 10, 11, 15, 27, 41, 48, 30,
         ],
     },
     {
@@ -60,7 +60,7 @@ export const stockManual = [
         name: 'Raphael',
         basedOn: '3s made',
         data: [
-            10, 15, 13,
+            10, 15, 13, 24,
         ],
     },
 
@@ -70,7 +70,7 @@ export const stockManual = [
         name: 'FreeBairn',
         basedOn: 'Stories',
         data: [
-            10, 11, 13, 9, 8, 21, 24, 27, 34, 52
+            10, 11, 13, 9, 8, 21, 24, 27, 34, 52, 43,
         ],
     },
 ];
