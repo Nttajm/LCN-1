15ed96db18be5d5711dc6ba5383ecd1ae744f460
Nttajm <130328868+Nttajm@users.noreply.github.com>
2024-11-14 18:44:32 -0800

    Refactor stock data management by moving stockManual to a separate file

---
+++ bp/inspo/overunder/stocks-array.js
@@ -0,0 +1,66 @@
+export const stockManual = [ 
+    // {
+    //     id: 'TRR',
+    //     sub: 'TorreCoin',
+    //     name: 'Torre',
+    //     basedOn: 'Test Scores and Moods',
+    //     data: [
+    //         30, 23, 2, 4, 5, 10, 15, 3, 34, 12, 2, 10, 13, 21,
+    //     ],
+    // },
+    {
+        id: 'MYY',
+        sub: 'myCoin',
+        name: 'Mylander',
+        basedOn: 'Taking phones',
+        data: [
+             20, 25, 43, 36, 32, 35, 50, 60, 12, 19, 28, 34
+        ],
+    },
+    {
+        id: 'WVR',
+        sub: 'WeaverCoin',
+        name: 'Weaver',
+        basedOn: 'Anger',
+        data: [
+           16, 5, 7, 10,  18.5, 9, 8, 6, 3, 5, 2, 1, 3, 2.5
+        ],
+    },
+    {
+        id: 'BRKS',
+        sub: 'SuckerCoin',
+        name: 'Burks',
+        basedOn: 'Yapping',
+        data: [
+             5, 6, 8, 9, 10, 12, 14, 56, 70, 61, 85, 87,
+        ],
+    },
+    {
+        id: 'MTT',
+        sub: 'matCoin',
+        name: 'Matt Ortiz',
+        basedOn: 'YELLING',
+        data: [
+            12, 14, 15, 16, 11, 8, 5, 7, 10, 11, 15, 21
+        ],
+    },
+    {
+        id: 'RPH',
+        sub: 'RaphCoin',
+        name: 'Raphael',
+        basedOn: '3s made',
+        data: [
+            10, 15, 13,
+        ],
+    },
+
+    // {
+    //     id: 'EGG',
+    //     sub: 'EggeringCoin',
+    //     name: 'Eggering',
+    //     basedOn: 'Wrong Notes',
+    //     data: [
+    //         40, 30, 20, 10, 8, 7, 5, 6, 8, 9, 10, 12, 14, 56, 70,
+    //     ],
+    // },
+];
--- bp/inspo/overunder/stocks.js
+++ bp/inspo/overunder/stocks.js
@@ -10,6 +10,8 @@ import {
     updateBalanceAdder,
 } from './global.js';
 
+import { stockManual } from './stocks-array.js';
+
 import { updateFb, getFb } from './firebaseconfig.js';
 
 getFb();
@@ -29,72 +31,7 @@ usrnamediv.innerHTML = userData.username || '???';
 checkBetsAndUpdateBalance();
 getFb();
 
-const stockManual = [ 
-    // {
-    //     id: 'TRR',
-    //     sub: 'TorreCoin',
-    //     name: 'Torre',
-    //     basedOn: 'Test Scores and Moods',
-    //     data: [
-    //         30, 23, 2, 4, 5, 10, 15, 3, 34, 12, 2, 10, 13, 21,
-    //     ],
-    // },
-    {
-        id: 'MYY',
-        sub: 'myCoin',
-        name: 'Mylander',
-        basedOn: 'Taking phones',
-        data: [
-             20, 25, 43, 36, 32, 35, 50, 60, 12, 19, 28, 34
-        ],
-    },
-    {
-        id: 'WVR',
-        sub: 'WeaverCoin',
-        name: 'Weaver',
-        basedOn: 'Anger',
-        data: [
-           16, 5, 7, 10,  18.5, 9, 8, 6, 3, 5, 2, 1, 3, 2.5
-        ],
-    },
-    {
-        id: 'BRKS',
-        sub: 'SuckerCoin',
-        name: 'Burks',
-        basedOn: 'Yapping',
-        data: [
-             5, 6, 8, 9, 10, 12, 14, 56, 70, 61, 85, 87,
-        ],
-    },
-    {
-        id: 'MTT',
-        sub: 'matCoin',
-        name: 'Matt Ortiz',
-        basedOn: 'YELLING',
-        data: [
-            12, 14, 15, 16, 11, 8, 5, 7, 10, 11, 15, 21
-        ],
-    },
-    {
-        id: 'RPH',
-        sub: 'RaphCoin',
-        name: 'Raphael',
-        basedOn: '3s made',
-        data: [
-            10, 15, 13,
-        ],
-    },
 
-    // {
-    //     id: 'EGG',
-    //     sub: 'EggeringCoin',
-    //     name: 'Eggering',
-    //     basedOn: 'Wrong Notes',
-    //     data: [
-    //         40, 30, 20, 10, 8, 7, 5, 6, 8, 9, 10, 12, 14, 56, 70,
-    //     ],
-    // },
-];
 
 function getLastPrice(stockName) {
     switch (stockName) {
@@ -655,3 +592,4 @@ async function renderLeaders() {
         console.error("Error fetching leaderboard data:", error);
     }
 }
+
