57be717b214260a464d8add5f128b1ecd5069123
Nttajm <130328868+Nttajm@users.noreply.github.com>
2024-11-15 12:37:04 -0800

    Update stocks-array.js

--- bp/inspo/overunder/stocks-array.js
+++ bp/inspo/overunder/stocks-array.js
@@ -5,7 +5,7 @@ export const stockManual = [
          name: 'Over Under',
          basedOn: 'over under in genral',
          data: [
-             43, 53, 104, 121, 99, 87, 106, 122, 132, 145, 190,
+             43, 53, 104, 121, 99, 87, 106, 122, 132, 145, 190, 150
          ],
     }, 
      
@@ -15,7 +15,7 @@ export const stockManual = [
          name: 'Torre',
          basedOn: 'Test Scores and Moods',
          data: [
-             30, 23, 2, 4, 5, 10, 15, 3, 34, 12, 2, 10, 13, 21, 46
+             30, 23, 2, 4, 5, 10, 15, 3, 34, 12, 2, 10, 13, 21, 46, 32
          ],
      },
     {
@@ -24,7 +24,7 @@ export const stockManual = [
         name: 'Mylander',
         basedOn: 'Taking phones',
         data: [
-            60, 12, 19, 28, 34, 54, 67, 87, 125, 142, 176, 163, 189, 151, 121, 109
+            60, 12, 19, 28, 34, 54, 67, 87, 125, 142, 176, 163, 189, 151, 121, 109, 96
         ],
     },
     {
@@ -33,7 +33,7 @@ export const stockManual = [
         name: 'Weaver',
         basedOn: 'sarcasm',
         data: [
-          10,  18.5, 9, 8, 6, 3, 5, 2, 1, 3, 2.3, 2, 3.5, 4.2, 4.6, 5.1, 7.5, 6, 9.5, 8, 7, 5, 8, 12, 6
+          1, 3, 2.3, 2, 3.5, 4.2, 4.6, 5.1, 7.5, 6, 9.5, 8, 7, 5, 8, 12, 6, 5.2
         ],
     },
     {
@@ -70,7 +70,7 @@ export const stockManual = [
         name: 'FreeBairn',
         basedOn: 'Stories',
         data: [
-            10, 11, 13, 9, 8, 21, 24, 27, 34, 52, 43, 62
+            10, 11, 13, 9, 8, 21, 24, 27, 34, 52, 43, 62, 54
         ],
     },
 ];
