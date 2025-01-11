3ded865bb6c10d67c4725bc8d088728e4677d979
Nttajm <130328868+Nttajm@users.noreply.github.com>
2025-01-10 18:37:39 -0800

    Update bets.js

--- bp/inspo/overunder/bets.js
+++ bp/inspo/overunder/bets.js
@@ -29,7 +29,7 @@ export const basketballPlayers = [
     techTeam: 'Tech High Basketball (V Boys)',
     against: 'Roseland',
     id: '30b',
-    amount: 41.5,
+    amount: 46.5,
     typeBet: 'Points',
     status: '',
     option: '',
