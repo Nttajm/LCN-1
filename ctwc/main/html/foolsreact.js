3f8947d938381a5f956af743fb83364be5c0b1f0
Nttajm <joelmulonde81@gmail.com>
2025-07-06 13:32:20 -0700

    chicken jockey

--- ctwc/main/html/foolsreact.js
+++ ctwc/main/html/foolsreact.js
@@ -1,24 +1,30 @@
 const navOther = document.querySelector('.other-links');
 const desktopNav = document.querySelector('.desktop-nav.nav-cont');
 
+document.addEventListener('DOMContentLoaded', () => {
+        
+    if (navOther && desktopNav) {
+    const fixLink = navOther.dataset.LinkFix || '';
+    const fixLInkDesktop = desktopNav.dataset.LinkFix || '';
+        navOther.innerHTML = `
+            <li><a href="${fixLink}index.html">Home</a></li>
+            <li><a href="${fixLink}about.html">About us</a></li>
+            <li><a href="${fixLink}events.html">Missions & Events</a></li>
+            <li><a href="${fixLink}give.html">Give</a></li>
+        `;
 
-if (navOther && desktopNav) {
-    navOther.innerHTML = `
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About us</a></li>
-        <li><a href="projects.html">Missions & Events</a></li>
-        <li><a href="give.html">Give</a></li>
-    `
-
-    desktopNav.innerHTML = `
-        <a href="index.html" class="logo">
-                    <img src="../images/logo-long-long.png" alt="CTWC">
+        desktopNav.innerHTML = `
+            <a href="${fixLInkDesktop}index.html" class="logo">
+                <img src="../images/logo-long-long.png" alt="CTWC">
                 </a>
                 <ul class="nav-links">
-                    <li><a href="index.html" class="cur uh">Home</a></li>
-                    <li><a href="about.html" class="cur uh">About us</a></li>
-                    <li><a href="projects.html" class="cur uh">Missions & Events</a></li>
-                    <li><a href="give.html" class="cur uh">Give</a></li>
+                <li><a href="${fixLInkDesktop}index.html" class="cur uh">Home</a></li>
+                <li><a href="${fixLInkDesktop}about.html" class="cur uh">About us</a></li>
+                <li><a href="${fixLInkDesktop}events.html" class="cur uh">Missions & Events</a></li>
+                <li><a href="${fixLInkDesktop}give.html" class="cur uh">wewo</a></li>
                 </ul>
-    `
-}
+        `;
+    } else {
+        console.log('Navigation elements not found');
+    }
+});
