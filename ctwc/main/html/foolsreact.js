46cdbbcad1ad431f2d804cd213413d224da1e8c2
Nttajm <joelmulonde81@gmail.com>
2025-07-11 05:28:22 -0700

    all the deer

--- ctwc/main/html/foolsreact.js
+++ ctwc/main/html/foolsreact.js
@@ -1,27 +1,38 @@
-const navOther = document.querySelector('.other-links');
-const desktopNav = document.querySelector('.desktop-nav.nav-cont');
 
 document.addEventListener('DOMContentLoaded', () => {
+    const navOther = document.querySelector('.other-links');
+    const desktopNav = document.querySelector('.desktop-nav.nav-cont');
         
     if (navOther && desktopNav) {
     const fixLink = navOther.dataset.LinkFix || '';
     const fixLInkDesktop = desktopNav.dataset.LinkFix || '';
-        navOther.innerHTML = `
-            <li><a href="${fixLink}index.html">Home</a></li>
+
+    const logoImg = document.querySelector('.logo')
+    logoImg.classList.add('fadeIn')
+    
+    navOther.innerHTML = `
             <li><a href="${fixLink}about.html">About us</a></li>
             <li><a href="${fixLink}events.html">Missions & Events</a></li>
+            <li>
+                <div class="extras">
+                    <div class='title'>
+                     <span>Missions</span>
+                    </div>
+                    
+                </div>
+            </li>
             <li><a href="${fixLink}give.html">Give</a></li>
         `;
 
         desktopNav.innerHTML = `
-            <a href="${fixLInkDesktop}index.html" class="logo">
+            <a href="${fixLInkDesktop}index.html" class="logo fadeIn">
                 <img src="images/logo-long-long.png" alt="CTWC">
                 </a>
-                <ul class="nav-links">
-                <li><a href="${fixLInkDesktop}index.html" class="cur uh">Home</a></li>
+                <ul class="nav-links fadeIn">
                 <li><a href="${fixLInkDesktop}about.html" class="cur uh">About us</a></li>
                 <li><a href="${fixLInkDesktop}events.html" class="cur uh">Missions & Events</a></li>
-                <li><a href="${fixLInkDesktop}give.html" class="cur uh">give</a></li>
+                <li><a href="${fixLInkDesktop}events.html" class="cur uh">Ministries</a></li>
+                <li><a href="${fixLInkDesktop}give.html" class="cur uh">Give</a></li>
                 </ul>
         `;
     } else {
