c7a2f38f43db2233611e5033941d09dfb257a6b3
Nttajm <130328868+Nttajm@users.noreply.github.com>
2026-03-01 22:58:01 -0800

    ialwayscomeback

---
+++ josu/data/songs.js
@@ -0,0 +1,87 @@
+import { songKeys } from './keys.js';
+
+export const songs = [
+    {
+        id: 1,
+        title: "Blinding Lights",
+        artist: "The Weeknd",
+        time: '00:07',
+        image: 'songs/theweeknd/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "Easy", speed: 1, mapper: "joelM", stars: 0.79,
+                songData: songKeys.blindingLights.easy
+            },
+            { name: "Normal", mapper: "joelM", stars: 1.20 },
+            { name: "Hard", mapper: "joelM", stars: 1.54 },
+        ]
+    },
+    { 
+        id: 6,
+        title: "Disparate Youth",
+        artist: "Santigold",
+        time: '3:44',
+        image: 'songs/disparate_youth/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "Normal", mapper: "joelM", stars: 1.75 },
+        ]
+    },
+    {
+        id: 2,
+        title: "iloveyou",
+        artist: "wiv.",
+        time: '4:15',
+        image: 'songs/iloveu/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "Easy", mapper: "joelM", stars: 1.03 },
+        ]
+    },
+    {
+        id: 11,
+        title: "nya arigato",
+        artist: "Leat'eq",
+        time: '4:15',
+        image: 'songs/nya_arigato/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "timas", mapper: "joelM", stars: 4.33 },
+        ]
+    },
+    {
+        id: 3,
+        title: "School Rooftop",
+        artist: "hisohkah",
+        time: '2:48',
+        image: 'songs/school_rooftop/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "nooby", mapper: "joelM", stars: 4.10 },
+            { name: "nooby", mapper: "joelM", stars: 6.51 },
+
+        ]
+    },
+    {
+        id: 4,
+        title: "Moment",
+        artist: "MIMI",
+        time: '3:20',
+        image: 'songs/ttto/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "Easy", mapper: "joelM", stars: 1.03 },
+        ]
+    },
+    {
+        id: 5,
+        title: "Dreaming",
+        artist: "Artist Name",
+        time: '4:02',
+        image: 'songs/ttto/main.png',
+        ranked: true,
+        difficulties: [
+            { name: "Hard", mapper: "joelM", stars: 3.50 },
+        ]
+    }
+]
